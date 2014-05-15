127ebc1f176f31206229dd695c68a2cdf66e7b48
Mark Howells-Mead <permanent.tourist@gmail.com>
2014-05-15 14:00:05 +0200

    Add initial code

---
+++ js/ui.js
@@ -0,0 +1,10 @@
+(function($){
+
+	$(document).ready(function(){
+		$('html').addClass('js');
+		$('head').append('<link rel="stylesheet" href="css/simple.css" type="text/css" />');
+		$('head').append('<link rel="stylesheet" href="css/responsive.css" type="text/css" media="(min-width:0)" />');
+		$('head').append('<link rel="stylesheet" href="css/jquery.mmenu.all.css" type="text/css" />');
+	});
+
+})(jQuery);
